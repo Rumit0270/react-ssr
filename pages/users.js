f63d0de726211bdf4f470a3f431cfed24c70590b
Rumit Tandukar <rumit0270@gmail.com>
2020-05-17 21:11:21 +0545

    update users

--- pages/users.js
+++ pages/users.js
@@ -13,7 +13,12 @@ const Users = (props) => {
             <div>
                 {
                     props.users.map(user => 
-                        <li key={user.id}> { user.name } </li>    
+                        <li key={user.id}>
+                            <Link href={`users/${user.id}`} >
+                            <button>{ user.name }</button>
+                            </Link>
+                        </li>  
+                      
                     )
                 }
             </div>
